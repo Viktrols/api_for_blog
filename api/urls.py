57b110e2c02f4daa4253d42ae5bb07af1641a289
Viktoria <viktrols@yandex.ru>
2021-05-11 17:55:03 +0300

    sprint 10 first commit

--- api/urls.py
+++ api/urls.py
@@ -19,5 +19,4 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(),
-         name='token_refresh'),
-    ]
+         name='token_refresh'),]
